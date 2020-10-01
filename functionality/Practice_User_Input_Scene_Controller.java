245a6df91a8c931b592a566fab06a1e32cfa3126
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2020-10-02 11:28:59 +1300

    Update speaker for practice

--- functionality/Practice_User_Input_Scene_Controller.java
+++ functionality/Practice_User_Input_Scene_Controller.java
@@ -1,16 +1,19 @@
 package functionality;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import GUI.GUI_Manager;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import util.Speaker;
 import util.Util;
 
 public class Practice_User_Input_Scene_Controller {
 	GUI_Manager gui_manager;
 	Stage window;
 	String original_notification;
+	
 
 	public Practice_User_Input_Scene_Controller(GUI_Manager gui_manager, Stage window) {
 		this.gui_manager = gui_manager;
@@ -80,23 +83,80 @@ public class Practice_User_Input_Scene_Controller {
 	public void set_speed_up_button() {
 		Button speed_up_button = gui_manager.get_practice_user_input_scene().get_speed_up_button();
 		speed_up_button.setOnAction(e -> {
-			// speeds up the espeaker
-			System.out.println("speed up");
+			String question = Util.practice_question_list.get(Util.practice_list_index);
+			String label = gui_manager.get_practice_user_input_scene().get_bottom_laebl_speaker().getText();
+			System.out.println(label);
+			String value = label.replace("Speaker (Current speed: x", "");
+			value = value.replace(")", "");
+			System.out.println(value);
+			double value_double = Double.parseDouble(value);
+			
+			if(value_double < 1.5) {
+				value_double = value_double + 0.1; 	
+			}
+			
+			String speed_val = Double.toString(value_double).substring(0,3);
+			
+			String output = "Speaker (Current speed: x" + speed_val + ")";
+			gui_manager.get_practice_user_input_scene().get_bottom_laebl_speaker().setText(output);
+			try {
+				if(Util.speaker_speed<50) {
+			Util.speaker_speed= Speaker.speedUp(question, Util.speaker_speed);
+				}
+				else {
+					Speaker.speedySpeak(question, Util.speaker_speed);
+				}
+			}catch (InterruptedException | IOException e1) {
+			e1.printStackTrace();
+		}
+			
 		});
 	}
 
 	public void set_speed_down_button() {
 		Button speed_down_button = gui_manager.get_practice_user_input_scene().get_speed_down_button();
 		speed_down_button.setOnAction(e -> {
-			// speeds down the espeaker
-			System.out.println("speed down");
+			Util.speaker_speed= Util.speaker_speed-10;
+			String label = gui_manager.get_practice_user_input_scene().get_bottom_laebl_speaker().getText();
+			System.out.println(label);
+			String value = label.replace("Speaker (Current speed: x", "");
+			value = value.replace(")", "");
+			System.out.println(value);
+			double value_double = Double.parseDouble(value);
+			
+			if(value_double > 0.5) {
+				value_double = value_double - 0.1; 	
+			}
+			
+			String speed_val = Double.toString(value_double).substring(0,3);
+			
+			String output = "Speaker (Current speed: x" + speed_val + ")";
+			gui_manager.get_practice_user_input_scene().get_bottom_laebl_speaker().setText(output);
+			String question = Util.practice_question_list.get(Util.practice_list_index);
+			try {
+				if(Util.speaker_speed>-50) {
+			Util.speaker_speed= Speaker.slowDown(question, Util.speaker_speed);
+				}
+				else {
+					Speaker.speedySpeak(question, Util.speaker_speed);
+				}
+			}catch (InterruptedException | IOException e1) {
+			e1.printStackTrace();
+		}
+			
+			System.out.println("speed down "+Util.speaker_speed);
 		});
 	}
 
 	public void set_repeat_button() {
 		Button repeat_button = gui_manager.get_practice_user_input_scene().get_repeat_button();
 		repeat_button.setOnAction(e -> {
-			// repeat the clue
+			String question = Util.practice_question_list.get(Util.practice_list_index);
+			try {
+				Speaker.speedySpeak(question, Util.speaker_speed);
+			} catch (InterruptedException | IOException e1) {
+				e1.printStackTrace();
+			}
 			System.out.println("repeat");
 
 		});
