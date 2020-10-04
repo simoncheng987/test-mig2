fc5df8c615154395e94aa7510ded21bf76c4640c
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2020-10-04 15:30:10 +1300

    Comments

--- util/Speaker.java
+++ util/Speaker.java
@@ -3,7 +3,9 @@ package util;
 import java.io.IOException;
 
 public class Speaker {
-
+        /**
+	 *  Use spd-say to achieve TTS. Takes a string that needs to be read out and an int that shows the speed.
+	 */
 	public static void speedySpeak(String sentence, int speed) throws InterruptedException, IOException {
 		String _speed = String.valueOf(speed);
 		String cmd = "spd-say -r" + _speed + " -p -20 --voice-type child_female -m some \"" + sentence + "\"";
@@ -16,7 +18,10 @@ public class Speaker {
 			System.out.println(e.getMessage());
 		}
 	}
-
+        /**
+	 *  Use spd-say to achieve TTS. Takes a string that needs to be read out and an int that shows the speed.
+	 *  Return an int indicating the speed after speed up.
+	 */
 	public static int speedUp(String sentence, int speed) throws InterruptedException, IOException {
 		String _speed = String.valueOf(speed + 10);
 		String cmd = "spd-say -r" + _speed + " -p -20 --voice-type child_female -m some \"" + sentence + "\"";
@@ -30,7 +35,10 @@ public class Speaker {
 		}
 		return speed + 10;
 	}
-
+         /**
+	 *  Use spd-say to achieve TTS. Takes a string that needs to be read out and an int that shows the speed.
+	 *  Return an int indicating the speed after speed down.
+	 */
 	public static int slowDown(String sentence, int speed) throws InterruptedException, IOException {
 		String _speed = String.valueOf(speed - 10);
 		String cmd = "spd-say -r" + _speed + " -p -20 --voice-type child_female -m some \"" + sentence + "\"";
