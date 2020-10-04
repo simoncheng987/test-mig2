1d416429e7c8a90e3b810c65ac1a380601c126e8
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2020-10-04 14:17:33 +1300

    Comments categories class

--- util/Categories.java
+++ util/Categories.java
@@ -1,14 +1,20 @@
 package util;
 
 import java.util.List;
-
+/**
+ *  Class created for storing different category objects.
+ *  Includes fields :name, question,answer,value_list and current index.
+ */
 public class Categories {
 	public String name;
 	public List<String> question;
 	public List<String> answer;
 	public List<Integer> value_list;
 	public int current_index;
-	
+	/**
+	*Constructor for Categories class, it takes three parameters as input:
+	*String Name, List<String> Q(questions list) and List<String>A(answers list).
+	*/
 	public Categories(String Name, List<String> Q, List<String> A) {
 		this.name = Name;
 		this.question = Q;
